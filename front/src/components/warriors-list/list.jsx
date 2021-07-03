cf5086c327a3449e7bb06d8952026008ebad2e26
lolarne <rne.lola@gmail.com>
2021-07-03 13:07:16 +0200

    get and display names within a list

--- front/src/components/warriors-list/list.jsx
+++ front/src/components/warriors-list/list.jsx
@@ -1,9 +1,31 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import {FETCH} from '../../Fetch.js';
 import './list.scss';
 
 const List = () =>{
+    const [warriors, setWarriors] = useState([]);
+
+    useEffect(()=>{
+        (async ()=>{
+            const getWarriors = await axios.get(`${FETCH}/warriors/get`);
+
+            if(getWarriors.data && JSON.stringify(warriors) !== JSON.stringify(getWarriors.data)){
+                setWarriors(getWarriors.data);
+            }
+        })()
+    }, [warriors]);
+
     return(
         <div>
             <h2>Membres de l&apos;équipage</h2>
+            <div>
+                <ul>
+                    {warriors.map((warrior, warriorKey) =>(
+                        <li key={warriorKey}>{warrior.name}</li>
+                    ))}
+                </ul>
+            </div>
         </div>
     )
 }
